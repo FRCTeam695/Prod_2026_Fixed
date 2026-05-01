ed18cb30a14deb45293174e89d836d4556508faa
Shamith Murthy <yhtrum7002@gmail.com>
2026-04-30 21:41:37 -0400

    half hub sweep auton (this broke the crashbar)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -440,9 +440,9 @@ public class RobotContainer {
     public Command halfHubSweep(){
       return  deadline(
                 swerve.driveToPoseWithSpeedLimit(() -> new Pose2d(10.84, 2.3, Rotation2d.fromDegrees(90)), 0.15, 3, 2.5)   //find this pose, right next to bump
-                .andThen(swerve.driveToPose(() -> new Pose2d(10.642, 3.459, Rotation2d.fromDegrees(90)), 0.2, 3.0)) //find this pose, get ready for pacman
-                .andThen(swerve.driveToPoseWithSpeedLimit(() -> new Pose2d(9.25, 3.15, Rotation2d.fromDegrees(-170)), 0.08, 2.5, 2.5))
-                .andThen(swerve.driveToPoseWithSpeedLimit(()-> new Pose2d(8.1, 2.6, Rotation2d.fromDegrees(-80)), 0.15, 3.0, 3.0))
+                .andThen(swerve.driveToPose(() -> new Pose2d(10.642, 3.859, Rotation2d.fromDegrees(90)), 0.2, 3.0)) //find this pose, get ready for pacman
+                .andThen(swerve.driveToPoseWithSpeedLimit(() -> new Pose2d(9.25, 3.859, Rotation2d.fromDegrees(-180)), 0.08, 2.5, 2.5))
+                .andThen(swerve.driveToPoseWithSpeedLimit(()-> new Pose2d(9.1, 2.6, Rotation2d.fromDegrees(-90)), 0.15, 3.0, 3.0))
                 .andThen(swerve.driveToPoseWithSpeedLimit(() -> new Pose2d(10.44, 2.5, Rotation2d.fromDegrees(12)), 0.15, 3.0, 3))   //find this pose, right next to bump
                 ,
                 pivot.goToPositionDegreesWithCondition(pivot.pivotExtendedPositionDegrees, pivot.withinTolerance).andThen(pivot.setDutyCycle(()-> -0.05)),             
